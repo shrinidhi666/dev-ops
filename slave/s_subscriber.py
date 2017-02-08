aea33814d0060a6d4a224837694829c546672069
shrinidhi <shrinidhi666@gmail.com>
2017-02-08 10:17:49 +0530

    fixing

--- slave/s_subscriber.py
+++ slave/s_subscriber.py
@@ -18,9 +18,69 @@ import lib.slave_utils
 import lib.processor
 import simplejson
 import requests
-import multiprocessing
+import tempfile
+import signal
+import psutil
+import fcntl
+import time
+
+app_lock_file = os.path.join(tempfile.gettempdir(),"devops-{0}.lock".format(os.environ['USER']))
+
+def receive_signal(signum, stack):
+  quit()
+
+signal.signal(signal.SIGTERM, receive_signal)
+signal.signal(signal.SIGINT, receive_signal)
+signal.signal(signal.SIGABRT, receive_signal)
+signal.signal(signal.SIGHUP, receive_signal)
+signal.signal(signal.SIGSEGV, receive_signal)
+
+def quit():
+  try:
+    os.remove(app_lock_file)
+  except:
+    lib.debug.error(sys.exc_info())
 
 
+def app_lock():
+  import random
+  time.sleep(random.uniform(0.000,0.500))
+  if(os.path.exists(app_lock_file)):
+    f = open(app_lock_file,"r")
+    pid = f.read().strip()
+    f.close()
+    try:
+      p = psutil.Process(int(pid))
+      lib.debug.info(p.cmdline()[1])
+      if(os.path.abspath(p.cmdline()[1]) == os.path.abspath(__file__)):
+        lib.debug.warning("already an instance of the app is running.")
+        lib.debug.warning("delete the file {0}".format(app_lock_file))
+        os._exit(1)
+      else:
+        raise Exception("seems like a different process has the same pid")
+    except:
+      lib.debug.warn(sys.exc_info())
+      f = open(app_lock_file,"w")
+      try:
+        fcntl.flock(f, fcntl.LOCK_EX | fcntl.LOCK_NB)
+      except:
+        lib.debug.error(sys.exc_info())
+        os._exit(1)
+      f.write(unicode(os.getpid()))
+      f.flush()
+      fcntl.flock(f, fcntl.LOCK_UN)
+      f.close()
+  else:
+    f = open(app_lock_file,"w")
+    try:
+      fcntl.flock(f, fcntl.LOCK_EX | fcntl.LOCK_NB)
+    except:
+      lib.debug.error(sys.exc_info())
+      os._exit(1)
+    f.write(unicode(os.getpid()))
+    f.flush()
+    fcntl.flock(f, fcntl.LOCK_UN)
+    f.close()
 
 
 
@@ -57,6 +117,7 @@ def start_sub(q=None):
 
 
 if __name__ == '__main__':
+  app_lock()
   register_host()
   start_sub()
 
