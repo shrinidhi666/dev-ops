17ecefb766b35864ae61e536c738742a491677bb
shrinidhi <shrinidhi666@gmail.com>
2017-02-24 11:45:07 +0530

    fixing

--- lib/transport.py
+++ lib/transport.py
@@ -147,7 +147,6 @@ class subscriber(object):
     else:
       self._socket_sub.setsockopt(zmq.SUBSCRIBE, bytes(unicode(self._topic)))
       lib.debug.debug("connecting to topic : " + str(self._topic))
-    time.sleep(2)
     fse = threading.Thread(target=self._fire_start_event)
     fse.start()
     while (True):
