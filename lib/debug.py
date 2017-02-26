4b80d003955597f2173065677ab3334abf2d0c66
Shrinidhi Rao K <shrinidhi666@gmail.com>
2017-02-26 14:21:41 +0530

    fixing

--- lib/debug.py
+++ lib/debug.py
@@ -12,4 +12,4 @@ sys.path.append(os.sep.join(os.path.abspath(__file__).split(os.sep)[:-2]))
 from logging import *
 
 FORMAT = "%(asctime)s : %(pathname)s : %(funcName)s - %(levelname)s - %(lineno)d - %(message)s"
-basicConfig(format=FORMAT, level=DEBUG)
+basicConfig(format=FORMAT, level=INFO)
