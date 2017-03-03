21eea877d90fff3c0144b649b76054d05a0a8408
shrinidhi <shrinidhi666@gmail.com>
2017-03-03 13:38:06 +0530

    fixing

--- lib/transport.py
+++ lib/transport.py
@@ -162,6 +162,7 @@ class subscriber(object):
           msg['hostid'] = lib.slave_utils.hostid()
           msg_to_send = simplejson.dumps(msg)
           self._socket_req = self._context.socket(zmq.REQ)
+          self._socket_req.setsockopt(zmq.SNDTIMEO, 1000)
           self._socket_req.connect("tcp://{0}:{1}".format(lib.config.slave_conf['master'], lib.config.slave_conf['master_ping_port']))
           self._socket_req.send_multipart([bytes(unicode(request_id)), bytes(unicode(state_name)),bytes(unicode(topic)), bytes(unicode(msg_to_send))])
           (request_id_recved) = self._socket_req.recv_multipart()
