df75d1cbcfd58464560c7c3f0174513040629a7e
shrinidhi <shrinidhi666@gmail.com>
2017-03-08 12:15:50 +0530

    fixing

--- bin/do-states.py
+++ bin/do-states.py
@@ -18,6 +18,7 @@ import lib.template
 import lib.master_utils
 import lib.db_sqlite3
 import simplejson
+import re
 
 parser = argparse.ArgumentParser(description="Command should be run on master")
 parser.add_argument("-l","--list",dest="list",action="store_true",help="list all the states that are available")
@@ -40,24 +41,26 @@ else:
     socket.connect("ipc:///tmp/publisher.zmq.sock")
     socket.send_multipart([uid,args.hosts,args.state])
     validhosts = socket.recv_pyobj()
+    socket.close()
     if(validhosts):
       for x in validhosts:
         print (x +" : "+ simplejson.dumps(validhosts[x],indent=4))
     else:
       print("No valid hosts")
+    if(not re.match('^ping\.',args.state)):
 
-    hosts_file = os.path.join(lib.constants.m_result_logs_dir,lib.constants.m_result_logs_prefix_hosts + lib.constants.m_result_logs_delimiter + uid)
-    hosts_file_fd = open(hosts_file,"w")
-    hosts_file_fd.write(simplejson.dumps(validhosts))
-    hosts_file_fd.flush()
-    hosts_file_fd.close()
+      hosts_file = os.path.join(lib.constants.m_result_logs_dir,lib.constants.m_result_logs_prefix_hosts + lib.constants.m_result_logs_delimiter + uid)
+      hosts_file_fd = open(hosts_file,"w")
+      hosts_file_fd.write(simplejson.dumps(validhosts))
+      hosts_file_fd.flush()
+      hosts_file_fd.close()
 
-    socket.close()
-    lib.db_sqlite3.execute("insert into log "
-                           "(request_id,state_name,topic) values "
-                           "(\"{0}\",\"{1}\",\"{2}\")".format(uid,args.state,args.hosts),
-                           db_file=lib.constants.m_dostates_sqlite3_file)
-    print("request id : "+ str(uid))
+
+      lib.db_sqlite3.execute("insert into log "
+                             "(request_id,state_name,topic) values "
+                             "(\"{0}\",\"{1}\",\"{2}\")".format(uid,args.state,args.hosts),
+                             db_file=lib.constants.m_dostates_sqlite3_file)
+      print("request id : "+ str(uid))
 
   elif(args.test):
     lib.debug.debug("testing state : " + args.test)
