3f0544d92998c1b268a9f2e3fa50f260feac79b3
shrinidhi <shrinidhi666@gmail.com>
2017-03-11 14:35:29 +0530

    fixing

--- lib/config.py
+++ lib/config.py
@@ -8,36 +8,15 @@ import os
 import sys
 sys.path.append(os.sep.join(os.path.abspath(__file__).split(os.sep)[:-2]))
 
-import lib.debug
 import lib.constants
 import yaml
-import glob
 
 if(os.path.exists(lib.constants.s_config_file)):
   __slave_fd = open(lib.constants.s_config_file, "r")
   slave_conf = yaml.safe_load(__slave_fd)
   __slave_fd.close()
-  # lib.debug.info("slave conf : ")
-  # lib.debug.info(slave_conf)
 
 if(os.path.exists(lib.constants.m_config_file)):
   __master_fd = open(lib.constants.m_config_file, "r")
   master_conf = yaml.safe_load(__master_fd)
   __master_fd.close()
-  # lib.debug.info("master conf : ")
-  # lib.debug.info(master_conf)
-
-
-# try:
-#   master_port = master_conf['master_rest_port']
-# except:
-#   lib.debug.warn("master.conf file doesnt have the option for master_rest_port")
-#   try:
-#     master_port = slave_conf['master_rest_port']
-#   except:
-#     lib.debug.warn("slave.conf file doesnt have the option for master_rest_port")
-#     master_port = 9491
-#
-#
-#
-
